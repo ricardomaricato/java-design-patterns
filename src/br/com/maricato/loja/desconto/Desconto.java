91759a519e3460d115175d4ecd48fcdde66ae02b
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-25 21:23:07 -0300

    feat: pattern template method

--- src/br/com/maricato/loja/desconto/Desconto.java
+++ src/br/com/maricato/loja/desconto/Desconto.java
@@ -8,9 +8,18 @@ public abstract class Desconto {
 
 	protected Desconto proximo;
 
-	public abstract BigDecimal calcular(Orcamento orcamento);
-
 	public Desconto(Desconto proximo) {
 		this.proximo = proximo;
 	}
+
+	public BigDecimal calcular(Orcamento orcamento) {
+		if (deveAplicar(orcamento)) {
+			return efetuarCalculo(orcamento);
+		}
+
+		return proximo.calcular(orcamento);
+	}
+
+	public abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+	public abstract boolean deveAplicar(Orcamento orcamento);
 }
