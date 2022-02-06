a65cca6c26a172079f7afda46d3947f30aa9684a
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-02-06 20:26:13 -0300

    pattern: Decorator

--- src/br/com/maricato/loja/TestesImpostos.java
+++ src/br/com/maricato/loja/TestesImpostos.java
@@ -10,6 +10,6 @@ public class TestesImpostos {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS(null)));
 	}
 }
